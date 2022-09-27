9119e3096335754d2a6087ab4a502687a805c144
onitseDv <onitsedv@hotmail.com>
2022-09-26 22:55:02 -0300

    commit

--- Rotas/rotas.js
+++ Rotas/rotas.js
@@ -11,16 +11,16 @@ const knex = require ('knex')({
 });
 
 
-const itemProduto = require ('../data/produtos.json'); //informações
+//const itemProduto = require ('../data/produtos.json'); //informações
 
 module.exports = rotas;
 
 //teste: http://localhost:3000/produtos/
 rotas.get('/produtos', (requisicao, resposta) => {
-    //return resposta.json(itemProduto)
     knex
         .select ('*')
         .from ('produto')
+        .orderBy('id')
         .then (results => {
             resposta.status(200).json(results)
         })
@@ -32,17 +32,7 @@ rotas.get('/produtos', (requisicao, resposta) => {
 
 //teste: http://localhost:3000/produtos/id-aqui
 rotas.get('/produtos/:id', (requisicao, resposta) => {
-    /*
-    const identificador = requisicao.params.id
-    for(let produto_search of itemProduto){
-        if(produto_search.id === id){
-            resposta.json(produto_search)
-            return
-        }
-    }
-    resposta.status(404).send('Produto não encontrado!')
-    */
-   let identificador = parseInt(requisicao.params.id)
+    let identificador = parseInt(requisicao.params.id)
     knex
         .select('*')
         .from('produto')
@@ -52,9 +42,8 @@ rotas.get('/produtos/:id', (requisicao, resposta) => {
                 let produto = results[0]
                 resposta.status(200).json(results)
             }else{
-                resposta.status(404).json({message: `Prroduto não encontrado`})
+                resposta.status(404).json({message: `Produto não encontrado`})
             }
-            
         })
         .catch (err => {
             resposta.status(500).json({message: `Erro ao obter produtos: ${err.message}`})
@@ -62,39 +51,9 @@ rotas.get('/produtos/:id', (requisicao, resposta) => {
 })
 
 
-//teste: POST pelo postman com o endpoint: http://localhost:3000/produtos
+//teste: POST pelo postman com o endpoint: http://localhost:3000/produtos/adicionarProduto
 //       com a mesma estrutura que o array possui
 rotas.post('/produtos/adicionarProduto', (requisicao, resposta) => {
-    /*
-    const body = requisicao.body
-    if(!body.descricao){
-        return resposta.status(400).end('Descrição do produto não pode ser nula!')
-    }else if(!body.valor){
-        return resposta.status(400).end('Valor do produto não pode ser nulo!')
-    }else if(!body.marca){
-        return resposta.status(400).end('Marca do produto não pode ser nula!')
-    }else{
-        //verificar se o produto existe, caso o retorno for nulo, insere
-        const result = itemProduto.find( desc => desc.descricao === body.descricao)
-        if(!result){
-            const maxID = itemProduto.reduce(function(prev, current) { 
-                return prev.id > current.id ? prev : current; 
-            })
-            //console.log(maxID.id)
-            const obj = {
-                id: maxID.id + 1,
-                descricao: body.descricao,
-                valor: body.valor,
-                marca: body.marca
-            }
-            //resposta.send(obj)
-            itemProduto.push(obj)
-            resposta.send(`Produto ${requisicao.body.descricao} adicionado`)
-        }else{
-            resposta.status(400).end(`Produto ${requisicao.body.descricao} já existente`)
-        }
-    }
-    */
     if(requisicao.body){
         knex ('produto') 
             .insert({ 
@@ -104,7 +63,7 @@ rotas.post('/produtos/adicionarProduto', (requisicao, resposta) => {
         }, ['id']) 
         .then((results) => { 
             let produtos = results[0] 
-            resposta.status(201).json({message: `Produto cadastrado com sucesso` })  
+            resposta.status(201).json({message: `Produto ${requisicao.body.descricao} cadastrado com sucesso` })  
             return 
         }) 
         .catch(err => { 
@@ -116,33 +75,40 @@ rotas.post('/produtos/adicionarProduto', (requisicao, resposta) => {
 
 //teste: http://localhost:3000/produtos/alteraProduto/id-aqui  
 rotas.put('/produtos/alteraProduto/:id', (requisicao, resposta) => {
-    const body = requisicao.body
-    //if(!body.id){
-    //    return resposta.status(400).end('ID do produto não pode ser nulo!')
-    if(!body.descricao){
-        return resposta.status(400).end('Descrição do produto não pode ser nula!')
-    }else if(!body.valor){
-        return resposta.status(400).end('Valor do produto não pode ser nulo!')
-    }else if(!body.marca){
-        return resposta.status(400).end('Marca do produto não pode ser nula!')
-    }else{
-        //itemProduto[requisicao.params.id - 1].id = requisicao.body.id;
-        itemProduto[requisicao.params.id - 1].descricao = requisicao.body.descricao;
-        itemProduto[requisicao.params.id - 1].valor = requisicao.body.valor;
-        itemProduto[requisicao.params.id - 1].marca = requisicao.body.marca;
-        return resposta.json(itemProduto[requisicao.params.id - 1]);
-    }
+    knex('produto')
+        .where('id', requisicao.params.id)
+        .update({
+            descricao: requisicao.body.descricao,  
+            valor: requisicao.body.valor,  
+            marca: requisicao.body.marca
+        })
+        .then(results => {
+            if(results.length == 0){
+                resposta.status(404).json({message: `Produto não encontrado`})
+            }else{
+                let produto = results[0]
+                resposta.status(200).json(({message: `Registros alterados ${results}`}))
+            }
+        }) 
+        .catch(err => { 
+            resposta.status(500).json({message: `Erro ao alterar produto: ${err.message}`}) 
+        })    
 })
 
 //teste: http://localhost:3000/produtos/deleta/id-aqui 
 rotas.delete('/produtos/deleta/:id', (requisicao, resposta) => {
-    const identificador = requisicao.params.id
-    let newitemProduto = itemProduto.filter(itemProduto => {
-        if(itemProduto.id !== identificador){
-            return true
-       }
-       return false
-    })
-    delete itemProduto[identificador - 1]
-    resposta.send(`Produto com id ${identificador} excluído!`) 
+    knex('produto')
+        .where('id', requisicao.params.id)
+        .del()
+        .then(results => { 
+            if(results.length == 0){
+                resposta.status(404).json({message: `Produto não encontrado`})
+            }else{
+                let produto = results[0]
+                resposta.status(200).json(({message: `Registros excluídos ${results}`}))
+            }
+        }) 
+        .catch (err => {
+            resposta.status(500).json({message: `Erro ao deletar produto: ${err.message}`})
+        })
 })
